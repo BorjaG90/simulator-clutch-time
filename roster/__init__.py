8861e3050e1c48bc29d22bb8747ddffca54ec379
BorjaG90 <borjagete90@gmail.com>
2019-01-09 15:27:39 +0100

    Added players & teams

---
+++ roster/__init__.py
@@ -0,0 +1,6 @@
+# -*- coding: utf-8 -*-
+
+__author__ = 'Borja Gete'
+__email__ = 'borjagete90@outlook.es'
+
+__all__ = ["roster"]
---
+++ roster/roster.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+
+__author__ = 'Borja Gete'
+__email__ = 'borjagete90@outlook.es'
+
+class Player:
+  def __init__(self,id_player):
+    self.id_player = int(id_player)
+class Roster:
+  def __init__(self, team, p1, p2, p3, p4, p5, p6, p7, p8, p9, p10, p11, p12):
+    self.team = int(team)
